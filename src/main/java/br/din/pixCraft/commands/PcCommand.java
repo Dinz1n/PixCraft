82b1746860ee27901f9c821cc6db7ee9f76b5610
Din <edesondacosta01@gmail.com>
2025-04-13 16:22:01 -0300

    GUI de confirmação de pagamento

--- src/main/java/br/din/pixCraft/commands/PcCommand.java
+++ src/main/java/br/din/pixCraft/commands/PcCommand.java
@@ -1,13 +1,20 @@
 package br.din.pixCraft.commands;
 
 import br.din.pixCraft.PixCraft;
-import br.din.pixCraft.order.OrderManager;
+import br.din.pixCraft.gui.ConfirmCancelGui;
+import br.din.pixCraft.payment.order.OrderManager;
 import br.din.pixCraft.payment.gateway.MercadoPagoAPI;
 import br.din.pixCraft.product.ProductManager;
 
 import org.bukkit.Bukkit;
 import org.bukkit.command.*;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.inventory.InventoryType;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -71,12 +78,14 @@ public class PcCommand implements CommandExecutor, TabCompleter {
             return true;
         }
 
-        String produto = args[1];
+        String product = args[1];
 
-        if (!ProductManager.getProducts().containsKey(produto)) {
+        if (!ProductManager.getProducts().containsKey(product)) {
             sender.sendMessage("§cEsse produto não existe.");
             return true;
         }
+
+        Player payer = null;
 
         if (sender instanceof Player) {
             Player player = (Player) sender;
@@ -87,7 +96,7 @@ public class PcCommand implements CommandExecutor, TabCompleter {
             }
 
             if (args.length == 2) {
-                OrderManager.processOrder(player, produto);
+                payer = player;
             } else {
                 sender.sendMessage("§cUso: /pc buy {produto}");
             }
@@ -95,13 +104,56 @@ public class PcCommand implements CommandExecutor, TabCompleter {
             if (args.length == 3) {
                 Player target = Bukkit.getPlayer(args[2]);
                 if (target != null) {
-                    OrderManager.processOrder(target, produto);
+                    payer = target;
                 } else {
                     sender.sendMessage("§cJogador não encontrado.");
                 }
             } else {
                 sender.sendMessage("§cUso: /pc buy {produto} {jogador}");
             }
+        }
+
+        if (payer != null) {
+            Player finalPayer = payer;
+            Listener listener = new Listener() {
+                @EventHandler
+                public void onInventoryClick(InventoryClickEvent event) {
+                    if (event.getClickedInventory().getType().equals(InventoryType.CHEST) && event.getView().getTitle().equals("Confirmar pedido")) {
+                        event.setCancelled(true);
+                        if (event.getWhoClicked() == finalPayer) {
+                            if (event.getCurrentItem() != null) {
+                                switch (event.getCurrentItem().getType()) {
+                                    case LIME_WOOL:
+                                        OrderManager.processOrder(finalPayer, product);
+                                        event.getWhoClicked().closeInventory();
+                                        HandlerList.unregisterAll(this);
+                                        break;
+                                    case RED_WOOL:
+                                        event.getWhoClicked().closeInventory();
+                                        HandlerList.unregisterAll(this);
+                                        break;
+                                    case NAME_TAG:
+                                        event.setCancelled(true);
+                                        break;
+                                    default:
+                                        event.setCancelled(true);
+                                        break;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                @EventHandler
+                public void onInventoryClose(InventoryCloseEvent event) {
+                    if (event.getInventory().getType().equals(InventoryType.CHEST) && event.getView().getTitle().equals("Confirmar pedido")) {
+                        HandlerList.unregisterAll(this);
+                    }
+                }
+            };
+            Bukkit.getPluginManager().registerEvents(listener, plugin);
+
+            ConfirmCancelGui.showGui(payer, ProductManager.getProduct(product));
         }
 
         return true;
