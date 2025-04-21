519a096e1aa6097684eea363e77df21ea4bde6bb
Din <edesondacosta01@gmail.com>
2025-04-21 18:35:48 -0300

    A classe também vai guardar o ícone do produto.

--- src/main/java/br/din/pixCraft/product/Product.java
+++ src/main/java/br/din/pixCraft/product/Product.java
@@ -1,28 +1,32 @@
 package br.din.pixCraft.product;
 
+import org.bukkit.inventory.ItemStack;
+
 import java.util.List;
 
 public final class Product {
     private final String productId;
-    private final String displayName;
+    private final String name;
     private final Double price;
     private final boolean tax;
     private final List<String> reward;
+    private final ItemStack icon;
 
-    public Product(String productId, String displayName, Double price, boolean tax, List<String> reward) {
+    public Product(String productId, String name, Double price, boolean tax, List<String> reward, ItemStack icon) {
         this.productId = productId;
-        this.displayName = displayName;
+        this.name = name;
         this.price = price;
         this.tax = tax;
         this.reward = reward;
+        this.icon = icon;
     }
 
     public String getProductId() {
         return productId;
     }
 
-    public String getDisplayName() {
-        return displayName;
+    public String getName() {
+        return name;
     }
 
     public Double getPrice() {
@@ -35,5 +39,9 @@ public final class Product {
 
     public List<String> getRewardCommands() {
         return reward;
+    }
+
+    public ItemStack getIcon() {
+        return icon;
     }
 }
