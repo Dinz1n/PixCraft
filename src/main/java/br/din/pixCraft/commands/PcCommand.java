7f97f68ae0072c1a6e9bac79ea5485a9bc1e9f4b
Din <edesondacosta01@gmail.com>
2025-04-21 18:37:21 -0300

    N/A

--- src/main/java/br/din/pixCraft/commands/PcCommand.java
+++ src/main/java/br/din/pixCraft/commands/PcCommand.java
@@ -1,20 +1,17 @@
 package br.din.pixCraft.commands;
 
 import br.din.pixCraft.PixCraft;
-import br.din.pixCraft.gui.ConfirmCancelGui;
+import br.din.pixCraft.gui.confirmcancel.CCGBuilder;
+import br.din.pixCraft.gui.confirmcancel.ConfirmCancelGui;
 import br.din.pixCraft.payment.order.OrderManager;
 import br.din.pixCraft.payment.gateway.MercadoPagoAPI;
+import br.din.pixCraft.product.Product;
 import br.din.pixCraft.product.ProductManager;
 
 import org.bukkit.Bukkit;
 import org.bukkit.command.*;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.HandlerList;
-import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.inventory.InventoryCloseEvent;
-import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.Inventory;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -23,9 +20,13 @@ import java.util.List;
 
 public class PcCommand implements CommandExecutor, TabCompleter {
     private final PixCraft plugin;
+    private final ProductManager productManager;
+    private final CCGBuilder ccgBuilder;
 
-    public PcCommand(PixCraft plugin) {
+    public PcCommand(PixCraft plugin, ProductManager productManager, CCGBuilder ccgBuilder) {
         this.plugin = plugin;
+        this.productManager = productManager;
+        this.ccgBuilder = ccgBuilder;
         plugin.getCommand("pixcraft").setExecutor(this);
         plugin.getCommand("pixcraft").setTabCompleter(this);
     }
@@ -78,9 +79,9 @@ public class PcCommand implements CommandExecutor, TabCompleter {
             return true;
         }
 
-        String product = args[1];
+        String productId = args[1];
 
-        if (!ProductManager.getProducts().containsKey(product)) {
+        if (!ProductManager.getProducts().containsKey(productId)) {
             sender.sendMessage("§cEsse produto não existe.");
             return true;
         }
@@ -114,46 +115,9 @@ public class PcCommand implements CommandExecutor, TabCompleter {
         }
 
         if (payer != null) {
-            Player finalPayer = payer;
-            Listener listener = new Listener() {
-                @EventHandler
-                public void onInventoryClick(InventoryClickEvent event) {
-                    if (event.getClickedInventory().getType().equals(InventoryType.CHEST) && event.getView().getTitle().equals("Confirmar pedido")) {
-                        event.setCancelled(true);
-                        if (event.getWhoClicked() == finalPayer) {
-                            if (event.getCurrentItem() != null) {
-                                switch (event.getCurrentItem().getType()) {
-                                    case LIME_WOOL:
-                                        OrderManager.processOrder(finalPayer, product);
-                                        event.getWhoClicked().closeInventory();
-                                        HandlerList.unregisterAll(this);
-                                        break;
-                                    case RED_WOOL:
-                                        event.getWhoClicked().closeInventory();
-                                        HandlerList.unregisterAll(this);
-                                        break;
-                                    case NAME_TAG:
-                                        event.setCancelled(true);
-                                        break;
-                                    default:
-                                        event.setCancelled(true);
-                                        break;
-                                }
-                            }
-                        }
-                    }
-                }
-
-                @EventHandler
-                public void onInventoryClose(InventoryCloseEvent event) {
-                    if (event.getInventory().getType().equals(InventoryType.CHEST) && event.getView().getTitle().equals("Confirmar pedido")) {
-                        HandlerList.unregisterAll(this);
-                    }
-                }
-            };
-            Bukkit.getPluginManager().registerEvents(listener, plugin);
-
-            ConfirmCancelGui.showGui(payer, ProductManager.getProduct(product));
+            Product product = ProductManager.getProduct(productId);
+            Inventory gui = ccgBuilder.build(product.getIcon());
+            new ConfirmCancelGui(payer, product, ccgBuilder, gui).showGui();
         }
 
         return true;
@@ -161,7 +125,8 @@ public class PcCommand implements CommandExecutor, TabCompleter {
 
     private void subCommandReload(CommandSender sender) {
         plugin.reloadConfig();
-        ProductManager.save();
+        productManager.reload();
+        ccgBuilder.reload();
         MercadoPagoAPI.setAccessToken(plugin.getConfig().getString("mercadopago.access-token"));
         sender.sendMessage("§aPlugin recarregado!");
     }
