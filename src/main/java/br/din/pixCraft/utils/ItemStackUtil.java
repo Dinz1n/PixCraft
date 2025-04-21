0dcaa5d57b680edfca8a96729c5a63867c861ac7
Din <edesondacosta01@gmail.com>
2025-04-21 18:42:05 -0300

    Novo método pra definir um PersistentData em um ItemStack.

--- src/main/java/br/din/pixCraft/utils/ItemStackUtil.java
+++ src/main/java/br/din/pixCraft/utils/ItemStackUtil.java
@@ -2,8 +2,10 @@ package br.din.pixCraft.utils;
 
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataContainer;
@@ -13,8 +15,8 @@ import java.util.List;
 import java.util.Objects;
 
 public class ItemStackUtil {
-    public static ItemStack create(Material material, String name, List<String> lore, int amount) {
-        ItemStack itemStack = new ItemStack(material, amount);
+    public static ItemStack create(Material material, String name, List<String> lore, int amount, boolean enchanted) {
+        ItemStack itemStack = new ItemStack(material == null? Material.BEDROCK : material, amount);
         ItemMeta itemMeta = itemStack.getItemMeta();
 
         if (name != null) {
@@ -24,9 +26,23 @@ public class ItemStackUtil {
         if (lore != null) {
             itemMeta.setLore(lore);
         }
+
+        if (enchanted) {
+            itemMeta.addEnchant(Enchantment.DURABILITY, 1, false);
+            itemMeta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES, ItemFlag.HIDE_ENCHANTS);
+        }
+
         itemStack.setItemMeta(itemMeta);
 
         return itemStack;
+    }
+
+    public static ItemStack setPersistentDataOnItemMeta(ItemStack itemStack, NamespacedKey key, PersistentDataType persistentDataType, Object valorToSet) {
+        ItemStack item = itemStack;
+        ItemMeta itemMeta = item.getItemMeta();
+        itemMeta.getPersistentDataContainer().set(key, persistentDataType, valorToSet);
+        item.setItemMeta(itemMeta);
+        return item;
     }
 
     public static void removeItemByData(Player player, NamespacedKey key, PersistentDataType persistentDataType, Object valorToCompare) {
