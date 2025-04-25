c87c8a5fd3f79b1339ac1f75416cfdb9609c3aac
Din <edesondacosta01@gmail.com>
2025-04-25 11:34:26 -0300

    Order agora armazena o QrCodeData e tem um novo método para cancelar a si mesmo? 🤔

--- src/main/java/br/din/pixCraft/payment/order/Order.java
+++ src/main/java/br/din/pixCraft/payment/order/Order.java
@@ -2,6 +2,10 @@ package br.din.pixCraft.payment.order;
 
 import br.din.pixCraft.payment.PaymentStatus;
 import br.din.pixCraft.product.Product;
+import com.mercadopago.client.payment.PaymentClient;
+import com.mercadopago.exceptions.MPApiException;
+import com.mercadopago.exceptions.MPException;
+import com.mercadopago.resources.payment.Payment;
 
 import java.util.Objects;
 import java.util.UUID;
@@ -9,14 +13,16 @@ import java.util.UUID;
 public final class Order {
     private final UUID playerUUID;
     private final Long paymentID;
+    private final String qrCodeData;
     private final Product product;
     private PaymentStatus status;
 
-    public Order(UUID playerUUID, Long paymentID, Product product) {
+    public Order(UUID playerUUID, Long paymentID, String qrCodeData, Product product, PaymentStatus status) {
         this.playerUUID = Objects.requireNonNull(playerUUID, "Player UUID não pode ser nulo");
         this.paymentID = Objects.requireNonNull(paymentID, "Payment ID não pode ser nulo");
+        this.qrCodeData = qrCodeData;
         this.product = Objects.requireNonNull(product, "Produto não pode ser nulo");
-        this.status = PaymentStatus.PENDING;
+        this.status = status;
     }
 
     public UUID getPlayerUUID() {
@@ -25,6 +31,10 @@ public final class Order {
 
     public Long getPaymentID() {
         return paymentID;
+    }
+
+    public String getQrCodeData() {
+        return qrCodeData;
     }
 
     public Product getProduct() {
@@ -37,5 +47,16 @@ public final class Order {
 
     public void setStatus(PaymentStatus status) {
         this.status = Objects.requireNonNull(status, "Status não pode ser nulo");
+    }
+
+    public void cancel() {
+        PaymentClient client = new PaymentClient();
+        try {
+            client.cancel(paymentID);
+        } catch (MPException e) {
+            throw new RuntimeException(e);
+        } catch (MPApiException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
