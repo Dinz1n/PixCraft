c113969ae0f332f26f248bef39836d91626ed112
Din <edesondacosta01@gmail.com>
2025-07-14 23:57:34 -0300

    add command and manager initialization in onEnable method

--- src/main/java/br/com/din/pixcraft/PixCraft.java
+++ src/main/java/br/com/din/pixcraft/PixCraft.java
@@ -1,18 +1,25 @@
 package br.com.din.pixcraft;
 
+import br.com.din.pixcraft.commands.PixCraftCommand;
+import br.com.din.pixcraft.order.OrderManager;
+import br.com.din.pixcraft.product.ProductManager;
 import br.com.din.pixcraft.utils.AsciiArtUtils;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PixCraft extends JavaPlugin {
     private static JavaPlugin instance;
-    private String pluginVersion;
 
     @Override
     public void onEnable() {
         instance = this;
         AsciiArtUtils.printAsciiArt(getLogger());
 
+        saveDefaultConfig();
 
+        ProductManager productManager = new ProductManager(this, "products.yml");
+        OrderManager orderManager = new OrderManager();
+
+        new PixCraftCommand(this, productManager, orderManager);
     }
 
     @Override
@@ -22,9 +29,5 @@ public final class PixCraft extends JavaPlugin {
 
     public static JavaPlugin getInstance() {
         return instance;
-    }
-
-    public static String getPluginVersion() {
-        return getPluginVersion();
     }
 }
