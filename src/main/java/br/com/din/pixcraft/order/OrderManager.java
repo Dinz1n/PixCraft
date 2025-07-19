8d4dc66a8bdd0c6514459c7ce20a07789ce92802
Din <edesondacosta01@gmail.com>
2025-07-19 17:47:27 -0300

    N/A

--- src/main/java/br/com/din/pixcraft/order/OrderManager.java
+++ src/main/java/br/com/din/pixcraft/order/OrderManager.java
@@ -1,7 +1,9 @@
 package br.com.din.pixcraft.order;
 
 import br.com.din.pixcraft.product.Product;
+
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -9,9 +11,18 @@ import java.util.UUID;
 
 public class OrderManager {
     private final Map<UUID, Order> orders = new HashMap();
+    private final JavaPlugin plugin;
+
+    public OrderManager(JavaPlugin plugin) {
+        this.plugin = plugin;
+    }
 
     public void processOrder(Player player, Product product) {
+        player.sendMessage("§aCriando pagamento...");
+    }
 
+    public void removeOrder(UUID uuid) {
+        orders.remove(uuid);
     }
 
     public Order getOrder(UUID uuid) {
