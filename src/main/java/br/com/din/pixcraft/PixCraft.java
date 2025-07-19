10641c4e108535aefd6d0b7083f4a67c49fd12e7
Din <edesondacosta01@gmail.com>
2025-07-19 17:45:31 -0300

    Organização na inicialização das classes importantes

--- src/main/java/br/com/din/pixcraft/PixCraft.java
+++ src/main/java/br/com/din/pixcraft/PixCraft.java
@@ -1,30 +1,38 @@
 package br.com.din.pixcraft;
 
-import br.com.din.pixcraft.commands.PixCraftCommand;
+import br.com.din.pixcraft.commands.PCCommand;
+import br.com.din.pixcraft.gui.ConfirmCancelGui;
 import br.com.din.pixcraft.order.OrderManager;
 import br.com.din.pixcraft.product.ProductManager;
-import br.com.din.pixcraft.utils.AsciiArtUtils;
+import br.com.din.pixcraft.utils.AsciiArt;
+
+import br.com.din.pixcraft.utils.minecraft_item_stack.PDCKeys;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PixCraft extends JavaPlugin {
     private static JavaPlugin instance;
+    private ProductManager productManager;
+    private ConfirmCancelGui confirmCancelGui;
+    private OrderManager orderManager;
 
     @Override
     public void onEnable() {
         instance = this;
-        AsciiArtUtils.printAsciiArt(getLogger());
+
+        AsciiArt.printAsciiArt(getLogger());
+        PDCKeys.init(this);
 
         saveDefaultConfig();
+        productManager = new ProductManager(this, "products.yml");
 
-        ProductManager productManager = new ProductManager(this, "products.yml");
-        OrderManager orderManager = new OrderManager();
+        confirmCancelGui = new ConfirmCancelGui();
+        orderManager = new OrderManager(this);
 
-        new PixCraftCommand(this, productManager, orderManager);
+        new PCCommand(this, productManager, confirmCancelGui, orderManager);
     }
 
     @Override
     public void onDisable() {
-
     }
 
     public static JavaPlugin getInstance() {
