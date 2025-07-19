ac01dcb64a465e882bed08c85d1de45d1c14bd37
Din <edesondacosta01@gmail.com>
2025-07-19 17:48:06 -0300

    Modelo de pedido

--- src/main/java/br/com/din/pixcraft/order/Order.java
+++ src/main/java/br/com/din/pixcraft/order/Order.java
@@ -1,4 +1,27 @@
 package br.com.din.pixcraft.order;
 
+import br.com.din.pixcraft.product.Product;
+
+import java.util.UUID;
+
 public class Order {
+    private final UUID id;
+    private final Product product;
+
+
+    public Order(UUID id, Product product) {
+        this.id = id;
+        this.product = product;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void cancel() {
+    }
 }
