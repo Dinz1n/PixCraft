586a891f79b26fa604021f7da93efaf75e2df1fc
Din <edesondacosta01@gmail.com>
2025-07-20 16:21:38 -0300

    Método simplificado

--- src/main/java/br/com/din/pixcraft/PixCraft.java
+++ src/main/java/br/com/din/pixcraft/PixCraft.java
@@ -4,9 +4,10 @@ import br.com.din.pixcraft.commands.PCCommand;
 import br.com.din.pixcraft.gui.ConfirmCancelGui;
 import br.com.din.pixcraft.order.OrderManager;
 import br.com.din.pixcraft.product.ProductManager;
-import br.com.din.pixcraft.utils.AsciiArt;
 
 import br.com.din.pixcraft.utils.minecraft_item_stack.PDCKeys;
+import org.bukkit.Bukkit;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PixCraft extends JavaPlugin {
@@ -19,7 +20,7 @@ public final class PixCraft extends JavaPlugin {
     public void onEnable() {
         instance = this;
 
-        AsciiArt.printAsciiArt(getLogger());
+        printAsciiArt();
         PDCKeys.init(this);
 
         saveDefaultConfig();
@@ -33,6 +34,26 @@ public final class PixCraft extends JavaPlugin {
 
     @Override
     public void onDisable() {
+    }
+
+    private void printAsciiArt() {
+        ConsoleCommandSender console = Bukkit.getConsoleSender();
+        String version = getDescription().getVersion();
+
+        String[] lines = {
+                "§c-----------------------",
+                "§a __",
+                "§a|__) | \\_/ §6by Din",
+                "§a|    | / \\ §6v" + version,
+                "§b __   __        ___ ___ ",
+                "§b/  ` |__)  /\\  |__   |  ",
+                "§b\\__, |  \\ /--\\ |     |  ",
+                "§c-----------------------"
+        };
+
+        for (String line : lines) {
+            console.sendMessage(line);
+        }
     }
 
     public static JavaPlugin getInstance() {
