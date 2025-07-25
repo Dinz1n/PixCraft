f165a44ba5b81175be2d92a1e2fc3a7dd31aa1ce
Din <edesondacosta01@gmail.com>
2025-07-25 12:08:54 -0300

    Enums para padronizar chaves e valores dos itens do gui

---
+++ src/test/java/Teste.java
@@ -0,0 +1,7 @@
+import br.com.din.pixcraft.gui.shop.ShopNBTKeys;
+
+public class Teste {
+    public static void main(String[] args) {
+        System.out.println(ShopNBTKeys.SHOP_ITEM_ID.name());
+    }
+}
